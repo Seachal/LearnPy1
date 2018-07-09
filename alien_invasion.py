3a9ea26456661a27858d733d4ad1cd7e66752ae4
Seachal <seachal.zhang@gmail.com>
2018-07-09 12:24:41 +0800

    重构
    重构：添加模块 game_functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,12 +2,15 @@
 # coding:utf-8
 ##  上面两条注释可以解决 中文注释报错的问题  SyntaxError: Non-ASCII character
 
-import sys
+
 
 import pygame
 
 from settings import Settings
 
+from ship import Ship
+
+import  game_functions as  gf
 
 # 一个函数
 def run_game():
@@ -18,21 +21,27 @@ def run_game():
     pygame.display.set_caption("Alien ")
 
     bg_color = (230, 230, 230)
-    # 开始游戏的主循环
 
+    # 创建一艘飞船
+
+    ship = Ship(screen)
+    # 开始游戏的主循环
     while True:
 
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         sys.exit()
+            gf.check_event(ship)
 
-            # 每次循环时都重绘屏幕
-            screen.fill(bg_color)
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
-
+            # # 每次循环时都重绘屏幕
+            # screen.fill(bg_color)
+            #
+            # ship.blitme()
+            #
+            # # 让最近绘制的屏幕可见
+            # pygame.display.flip()
+            gf.update_screen(ai_settings,screen,ship)
 
 # run_game()必须前面没有缩进，否则 pygame.display.flip()后面会报错，看来python虽然没有{}，但是是靠 缩进来比对代码块。
 run_game()
---
+++ game_functions.py
@@ -0,0 +1,27 @@
+# !/usr/bin/python
+# coding:utf-8
+##  上面两条注释可以解决 中文注释报错的问题  SyntaxError: Non-ASCII character
+
+import  sys
+
+import  pygame
+
+def  check_event(ship):
+    # 监视键盘和鼠标事件
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+             if event.key == pygame.K_RIGHT:
+                 #向右移动飞船
+                 ship.rect.centerx += 1
+
+def update_screen(ai_settings,screen,ship):
+    """更新屏幕上的图像，并切换到主屏幕"""
+    # 每次循环时都重绘屏幕
+    screen.fill(ai_settings.bg_color) # ai_settings.bg_color可以点击进去，真的好神奇，就好像(ai_settings,screen,ship)是Java的(Settings ai_settings,screen,ship)一样
+
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
